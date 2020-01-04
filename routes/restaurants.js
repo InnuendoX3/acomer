09e9edca2ee5ae09f21e21eeb12cc5d99e5fcd54
Johan Garzon <johanprograma@hotmail.com>
2020-01-04 09:39:25 +0100

    Add some Mock-restaurants-data

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const restaMock = require('../utils/resta-mocks');
 
 router.get('/', (req, res) => {
-   res.send('<h1>Ruta GET de restautantes</h1>');
+   // res.send('<h1>Ruta GET de restautantes</h1>');
+   res.send(restaMock);
 });
 
 router.post('/', (req, res) => {
    res.send('Ruta POST de restaurantes');
+});
+
+router.put('/', (req, res) => {
+   res.send('Ruta PUT de restaurantes');
+});
+
+router.delete('/', (req, res) => {
+   res.send('Ruta DELETE de restaurantes');
 });
 
 module.exports = router;
