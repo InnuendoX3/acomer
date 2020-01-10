b4cf37e86aa5c51ab598ea90e73413d9d6b6bc24
Johan Garzon <johanprograma@hotmail.com>
2020-01-10 22:41:52 +0100

    Testing saving form-register data to DB: OK

--- routes/credentials.js
+++ routes/credentials.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const restarurantsDB = require('../db/connection');
+const restaurantsDB = require('../db/connection');
 
 
 router.get('/register', (req, res) => {
    res.render('credentials/register')
+})
+
+router.post('/register', (req, res) => {
+   let name = req.body.name;
+   let email = req.body.email;
+   let password = req.body.password;
+
+   let quer = `INSERT INTO users (name, email, password) VALUES ("${name}","${email}","${password}")`
+
+   restaurantsDB.query(quer, (err, rows) => {
+      if (err) {
+         console.log(err)
+      } else {
+         res.redirect('/login')
+      }
+   })
+   
 })
 
 
